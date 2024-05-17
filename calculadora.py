d1046ed13175adf7ad6fb4f57c62be6181a14fd7
lucasrafaelx <lucasrafaelxx@gmail.com>
2024-05-17 00:02:16 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,10 @@
+Valor1= int(input(" Digite o Valor1 "))
+Valor2= int(input(" Digite o Valor2 "))
+adição= Valor1+Valor2
+subtração= Valor1-Valor2
+multiplicação= Valor1*Valor2
+divisão= Valor1/Valor2
+print("Resultado da adição:"+ str(adição))
+print("Resultado da subtração:"+ str(subtração))
+print("Resultado da multiplicação:"+ str(multiplicação))
+print("Resultado da divisão:"+ str(divisão))
